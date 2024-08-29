82a301647fa0dc7dd27290d289492e6610afd25f
Andrei Gota <andreigota2003@gmail.com>
2024-08-29 20:54:56 +0300

    .

--- flappybird.py
+++ flappybird.py
@@ -383,5 +383,5 @@ def main():
 
 if __name__ == '__main__':
     # If this module had been imported, __name__ would be 'flappybird'.
-    # It was executed (e.g. by double-clicking the file), so call main.
+    # It was executed (e.g. by double-clicking the file), so call main
     main()
